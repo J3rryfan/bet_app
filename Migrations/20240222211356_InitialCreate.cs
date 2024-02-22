91573a2a57222ae2dfa4d8903a3067e63d55d128
Jerry Fan <Jerryfan71@gmail.com>
2024-02-22 13:19:16 -0800

    inital commit

---
+++ Migrations/20240222211356_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BetApp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Item = table.Column<string>(type: "text", nullable: false),
+                    HighestBid = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Username" },
+                values: new object[,]
+                {
+                    { 1, "John Doe" },
+                    { 2, "Jane Doe" },
+                    { 3, "Jerry Fan" },
+                    { 4, "Michael" },
+                    { 5, "Scott" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Bets",
+                columns: new[] { "Id", "HighestBid", "Item", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 100, "Shoes", 1 },
+                    { 2, 200, "Ring", 2 },
+                    { 3, 300, "Telephone", 3 },
+                    { 4, 400, "Milk", 4 },
+                    { 5, 500, "pants", 5 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bets_UserId",
+                table: "Bets",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using BetApp.Web.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+DotNetEnv.Env.Load();
+
+
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
+
+
+
+builder.Services.AddDbContext<DatabaseContext>(
+  opt =>
+  {
+    opt.UseNpgsql(connectionString);
+    if (builder.Environment.IsDevelopment())
+    {
+      opt
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors();
+    }
+  }
+);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
