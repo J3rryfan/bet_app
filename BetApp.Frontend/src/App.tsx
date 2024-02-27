37be8e6f130321dbdec5a715f71d9b60f8c3ee17
Jerry Fan <Jerryfan71@gmail.com>
2024-02-27 00:14:57 -0800

    install microsoftSinglaR from the frontend and connect the connection from the backend to the frontend

--- BetApp.Frontend/src/App.tsx
+++ BetApp.Frontend/src/App.tsx
@@ -2,8 +2,12 @@ import { Button } from "@/components/ui/button"
 import { FlagIcon } from "lucide-react"
 import { Input } from "./components/ui/input"
 
+import useSignalR from "./hook/useSignalR"
+
+
 
 export default function App() {
+  const { connection } = useSignalR("/r/betHub");
   return (
     <div className="flex flex-col min-h-screen">
       <header className="bg-gray-50 py-2 border-b dark:bg-gray-900">
@@ -13,6 +17,11 @@ export default function App() {
         </div>
       </header>
 
+      <div>
+        <h1>SignalR Bet</h1>
+        <p>{connection ? "✅" : "❌"}</p>
+      </div>
+      
 
       <main className="flex-1 grid items-center justify-center min-h-[calc(100vh_-_3rem)] py-10">
         <div className="container flex flex-col items-center px-4 md:px-6 space-y-4">
